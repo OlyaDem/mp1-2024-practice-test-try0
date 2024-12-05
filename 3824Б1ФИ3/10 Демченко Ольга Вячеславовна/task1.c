094eefea6dd7bbee465bb8f1b9063b9b25d3ad6b
OlyaDem <olga.ru.nn@icloud.com>
2024-12-05 09:37:42 +0300

    Update task1.c
    Демченко Ольга Вячеславовна

--- 3824Б1ФИ3/10 Демченко Ольга Вячеславовна/task1.c
+++ 3824Б1ФИ3/10 Демченко Ольга Вячеславовна/task1.c
@@ -3,5 +3,11 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+  int rez = 0;
+  if ((INT_MIN < (a + b)) && ((a + b) < INT_MAX)) rez = 0;
+  else {
+    if ((a + b) <= INT_MIN) rez = INT_MIN;
+    if ((a + b) >= INT_MAX) rez = INT_MAX;
+  }
+  return rez;
 }
